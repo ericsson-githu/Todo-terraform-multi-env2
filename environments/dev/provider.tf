04f3609bcca2e5895d6661fb11688f89add4a09e
uday pratap singh <pruday32@gmail.com>
2025-10-07 00:04:42 +0530

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -16,4 +16,6 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c8ae0155-19b9-4306-a16f-ce3e1dab29f4"
+   client_id  = "2e5716e1-21ef-403b-8b39-fa79c7ae2f4a"
+   tenant_id  ="ba820411-fb17-40b7-aa1e-e6bb2ba7b2da"
 }
