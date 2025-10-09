328372dec382a7f209055ae04b9849cd3184c5b0
uday pratap singh <pruday32@gmail.com>
2025-10-09 12:51:04 +0530

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -8,12 +8,12 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "aavyay-rg"
     storage_account_name = "aavyaystorage987321"
-    container_name       = "tfstate1"
+    container_name       = "tfstate"
     key                  = "tev.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "c8ae0155-19b9-4306-a16f-ce3e1dab29f4" 
+  subscription_id = "70263326-482d-49df-9d08-fa285b859b10" 
 }
